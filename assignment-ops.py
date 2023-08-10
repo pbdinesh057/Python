60dd6d65cc98ebb5119fbc3ab2e21b2346f88c82
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-10 23:56:49 +0530

    Added Assign-ops

---
+++ assignment-ops.py
@@ -0,0 +1,22 @@
+name = "Dinesh"
+age = 24
+Male = True
+
+print(name)
+print(age)
+print(Male)
+
+
+names, ages, Males = "Harish", 22,True
+
+print(names,ages,Males)
+
+
+#if same value is there we can assign like below
+A=B=C=D=10
+#Normal way
+# A = 10
+# B = 10
+# C = 10
+# D = 10
+print(A, B, C, D)
