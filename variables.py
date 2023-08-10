92b03f2197a29638379f9810de072b48a7c0392c
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-10 23:56:49 +0530

    Added Variables.py

--- variables.py
+++ variables.py
@@ -3,3 +3,25 @@ Last_name = "Pinnepalli"
 full_name = first_name +" "+Last_name
 print("Hello "+ full_name)
 print(type(full_name))
+
+
+
+age = 21
+age = age + 1
+age+=2
+age+=22
+#print("Age is"+ age) - This wont work as we are combining 2 data types/
+#for that we need to fo type casting
+print("Age is " +str(age))
+print(type(age))
+
+
+height = 250.5
+print("Your Height is "+str(height)+" cms")
+
+
+#Boolean are used when we get to if statements to check is some statement is True/False
+
+human=True
+print(human)
+print("Are you a Human?  "+str(human))
