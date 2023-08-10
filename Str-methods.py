8f8ddd03f5b814fda960b9593ac8f95cb6d81d7f
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-11 00:23:39 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	variables.py

---
+++ Str-methods.py
@@ -0,0 +1,14 @@
+name = "Bro Code"
+
+print(len(name))
+print(name.find("o"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isupper())
+print(name.islower())
+print(name.isalpha())
+print(name.isalnum())
+print(name.replace("o","a"))
+print(name*3)
