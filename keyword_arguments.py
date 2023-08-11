0eecaa00fb23da1eda8faaa5317c1446d4529cbd
pbdinesh057 <pbdinesh057@gmail.com>
2023-08-11 18:37:00 +0530

    keyword-arguments.py
    Signed-off-by: pbdinesh057 <pbdinesh057@gmail.com>

---
+++ keyword_arguments.py
@@ -0,0 +1,9 @@
+#Arguments preceded by identifier when we pass them to a function.
+#The order of arguments doesn't matter, unlike positional arguments
+
+
+
+def hello(first,middle,last):
+    print("Hello "+first+" "+middle+" "+last)
+
+hello(last="Code",first="Dinesh",middle="Pinnepalli")
